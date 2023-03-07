8135c3c4c966a8e4ce2c59244ce2f1337422ce0a
jmarquezba <124936850+jmarquezba@users.noreply.github.com>
2023-03-06 21:28:12 -0500

    Si se puede

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -9,12 +9,8 @@ public class Asignatura {
     public Tipo tipo;
 
     public Asignatura() {
-        this("Sin nombre",Tipo.FUNDAMENTACION);
-        this(0);
-    }
-
-    public Asignatura(int codigoInterno,Tipo tipo) {
-        this("Sin nombre", codigoInterno, 0,tipo);
+        this("Sin nombre", Tipo.FUNDAMENTACION);
+        codigoExterno = 0;
     }
 
     public Asignatura(int codigoExterno,Tipo tipo) {
@@ -39,9 +35,7 @@ public class Asignatura {
         this.tipo = tipo;
     }
 
-    public void cambiarDatos(int codigoInterno) {
-        this.codigoInterno = codigoInterno;
-    }
+
 
     public void cambiarDatos(int codigoExterno) {
         this.codigoExterno = codigoExterno;
--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -8,7 +8,7 @@ public class Persona {
     
     static {
         totalPersonas = 0;
-        cedula = 3;
+
     }
 
     public Persona(long cedula, String nombre) {
@@ -16,12 +16,13 @@ public class Persona {
         this.nombre = nombre;
         totalPersonas++;
     }
-    
+
     public Persona(String nombre, long cedula) {
         this.cedula = cedula;
         this.nombre = nombre;
         totalPersonas++;
     }
+    
 
     public Persona(long cedula) {
         this.cedula = cedula;
@@ -31,6 +32,12 @@ public class Persona {
 
     public Persona(String nombre) {
         this.nombre = "";
+        cedula=1;
+        totalPersonas++;
+    }
+
+    public Persona(){
+        cedula=0;
         totalPersonas++;
     }
     
